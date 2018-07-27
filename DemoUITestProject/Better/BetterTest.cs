ee4af03728167480447670c8ef380cb556a96b36
Peter Simons <peter.simons@solarwinds.com>
2018-07-27 14:58:56 +0100

    Ressolved Project migration issues

--- DemoUITestProject/Better/BetterTest.cs
+++ DemoUITestProject/Better/BetterTest.cs
@@ -1,28 +1,12 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Xamarin.UITest;
-using Xamarin.UITest.Queries;
 
 namespace UITestDemo.UITest
 {
-    [TestFixture(Platform.Android)]
-    [TestFixture(Platform.iOS)]
-    public class BetterTests
+    public class BetterTests : Setup.BaseTests
     {
-        IApp app;
-        Platform platform;
-
-        public BetterTests(Platform platform)
+        public BetterTests(Platform p) : base(p)
         {
-            this.platform = platform;
-        }
-
-        [SetUp]
-        public void BeforeEachTest()
-        {
-            app = AppInitializer.StartApp(platform);
         }
 
         [Test]
